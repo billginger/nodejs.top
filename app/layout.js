2cb9d9bf4e78f98871fba0ac2073a9cbe89e56f0
Bill <jfg@163.com>
2023-07-07 11:43:43 +0800

    Update html lang to zh-Hans

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="zh-Hans">
       <body>{children}</body>
     </html>
   )
