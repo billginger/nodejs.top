401e6cf084b501dfdb0fc73ea3ff74e07911b5f8
Bill <jfg@163.com>
2023-07-07 20:02:20 +0800

    Update page structure and style

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,25 @@
 import './globals.css'
 
+const RootLayout = ({ children }) => (
+  <html lang="zh-Hans">
+    <body>
+      <header>
+        <h1>nodejs.top</h1>
+      </header>
+      {children}
+      <footer>
+        <p>&copy; 2023 nodejs.top</p>
+        <p>
+          <a href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备19003000号</a>
+        </p>
+      </footer>
+    </body>
+  </html>
+)
+
 export const metadata = {
   title: 'Nodejs.top',
   description: 'A technology sharing site',
 }
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="zh-Hans">
-      <body>{children}</body>
-    </html>
-  )
-}
+export default RootLayout
