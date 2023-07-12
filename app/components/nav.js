e5f7055fcebaa205cd4550f97f999a80fa577f86
Fenggong Jiang <jfg@163.com>
2023-07-12 21:28:25 +0800

    Improve Nav component

--- app/components/nav.js
+++ app/components/nav.js
@@ -11,14 +11,13 @@ const navLinks = [
 
 const Nav = () => {
   const pathname = usePathname()
+  const classActive = 'inline-block p-2 m-1 bg-[#00000033]'
+  const classNormal = 'inline-block p-2 m-1 hover:bg-[#ffffff11] hover:shadow-lg'
   return (
     <nav>
       {navLinks.map(link => {
-        const isActive = link.href == '/' && link.href == pathname || link.href != '/' && pathname.startsWith(link.href)
-        let className = 'inline-block p-2 m-1 hover:bg-[#ffffff11] hover:shadow-lg'
-        if (isActive) {
-          className = 'inline-block p-2 m-1 bg-[#00000033]'
-        }
+        const isActive = link.href == '/' && pathname == '/' || link.href != '/' && pathname.startsWith(link.href)
+        const className = isActive ? classActive : classNormal
         return (
           <Link className={className} href={link.href} key={link.name}>{link.name}</Link>
         )
