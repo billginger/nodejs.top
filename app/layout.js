2e3fe8ecab3e190364bf668f4be0cb42b760ec58
Fenggong Jiang <jfg@163.com>
2023-07-13 15:48:24 +0800

    Update layout  style

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ const RootLayout = ({ children }) => (
         </div>
       </header>
       <main className="container mx-auto px-6 pb-12 -mt-16">
-        <div className="bg-[#fff] p-16 shadow-lg">
+        <div className="bg-white p-16 shadow-lg">
           {children}
         </div>
       </main>
