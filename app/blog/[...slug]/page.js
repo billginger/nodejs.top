f16669610e2d32df9f361cbe65fb1de1d846658a
Bill <jfg@163.com>
2023-07-18 11:23:43 +0800

    Update posts lib

--- app/blog/[...slug]/page.js
+++ app/blog/[...slug]/page.js
@@ -1,20 +1,30 @@
-import { getPostBySlug } from '/lib/posts'
+import { getSlugs, getPostBySlug } from '/lib/posts'
 import Category from '../../components/category'
 
 const Post = async ({ params }) => {
   const { slug } = params
-  const { title, category, body } = await getPostBySlug(slug)
+  const { title, category, content } = await getPostBySlug(slug)
   const date = slug[0]
-  const content = body.replace(/<a href="http/g, '<a target="_blank" href="http')
+  const html = content.replace(/<a href="http/g, '<a target="_blank" href="http')
   return (
     <>
       <h1 className="text-2xl text-center font-[MarcellusSC]">{title}</h1>
       <div className="text-sm text-center text-neutral-500 my-2">
         <Category>{category}</Category> / {date}
       </div>
-      <div className="my-8" dangerouslySetInnerHTML={{ __html: content }} />
+      <div className="my-8" dangerouslySetInnerHTML={{ __html: html }} />
     </>
   )
 }
+
+export const generateMetadata = async ({ params }) => {
+  const { slug } = params
+  const { title } = await getPostBySlug(slug)
+  return {
+    title: `${title} - Nodejs.top`,
+  }
+}
+
+export const generateStaticParams = () => getSlugs()
 
 export default Post
